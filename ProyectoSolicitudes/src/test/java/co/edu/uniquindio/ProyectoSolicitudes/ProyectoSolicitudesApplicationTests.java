8c1552f295ccb9e9b0e7de2b810432fe67d3fab1
JenniferCortes-25 <tu_correo_de_github@ejemplo.com>
2026-03-04 22:34:17 -0500

    Initial commit

---
+++ ProyectoSolicitudes/src/test/java/co/edu/uniquindio/ProyectoSolicitudes/ProyectoSolicitudesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.ProyectoSolicitudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSolicitudesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
